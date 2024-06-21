e2912e5e96d681907ff26ba4664ba0d1789707f0
Aditya Shourya <46932291+adishourya@users.noreply.github.com>
2024-06-22 00:19:39 +0200

    init thread colors

---
+++ lua/dark_aquarium/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+local theme = require('dark_aquarium.theme')
+
+M.setup = function()
+  vim.cmd('hi clear')
+
+  vim.o.background = 'dark'
+  if vim.fn.exists('syntax_on') then
+    vim.cmd('syntax reset')
+  end
+
+  vim.o.termguicolors = true
+  vim.g.colors_name = 'dark_aquarium'
+
+  theme.set_highlights()
+end
+
+return M
---
+++ lua/dark_aquarium/palette.lua
@@ -0,0 +1,58 @@
+local colors = {
+  white = "#ced4df",
+  darker_black = "#19191a",
+  black = "#1C1C1E",
+  black2 = "#222225",
+
+  one_bg = "#2c2c2f",
+  one_bg2 = "#36363a",
+  one_bg3 = "#404045",
+
+  grey = "#4a4a4f",
+  grey_fg = "#54545a",
+  grey_fg2 = "#5e5e64",
+  light_grey = "#67676f",
+
+  base00 = "#1C1C1E",
+  base01 = "#222225",
+  base02 = "#2c2c2f",
+  base03 = "#36363a",
+  base04 = "#404045",
+
+
+  base05 = "#bac0cb",
+  base06 = "#c5cbd6",
+  base07 = "#ced4df",
+  base08 = "#ebb9b9",
+  base09 = "#e8cca7",
+  base0A = "#e6dfb8",
+  base0B = "#b1dba4",
+  base0C = "#b8dceb",
+  base0D = "#a3b8ef",
+  base0E = "#f6bbe7",
+  base0F = "#eAc1c1",
+
+  red = "#ebb9b9",
+  baby_pink = "#EAC1C1",
+  pink = "#E9D1D1",
+  line = "#2d2d37",
+  green = "#b1dba4",
+  vibrant_green = "#BEE0A8",
+  blue = "#CDDBF9",
+  nord_blue = "#BCCAEB",
+  yellow = "#E6DFB8",
+  sun = "#EEE8BA",
+  purple = "#f6bbe7",
+  dark_purple = "#E8B6E9",
+  teal = "#AEDCB7",
+  orange = "#E8CCA7",
+  cyan = "#b8dceb",
+  statusline_bg = "#222225",
+  lightbg = "#2e2e38",
+  pmenu_bg = "#ebb9b9",
+  folder_bg = "#b8dceb",
+
+
+}
+
+return colors
